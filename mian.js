39662f7e5e11b98e1b6c8d735f90dec4f09d670b
zty <782094766@qq.com>
2019-05-26 23:26:58 +0800

    3

--- mian.js
+++ mian.js
@@ -45,7 +45,7 @@ function drawCircle(x, y, radius) {
 
 function drawLine(x1, y1, x2, y2) {
   context.beginPath();
-  context.strokeStyle = 'black'
+  context.strokeStyle = 'blue'
   context.moveTo(x1,                y1) // 起点
   context.lineWidth = 5
   context.lineTo(x2, y2) // 终点
